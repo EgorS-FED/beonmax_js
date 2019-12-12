a49ba6afc317c3f14a104c73468b16b43eda81ae
EgorS-FED <egors-2020@mail.ru>
2019-12-12 10:11:08 +0300

    correct script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-// let expense_1 = prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 1");
-// let expenseAnswer_1 = prompt("Во сколько обойдется", "Сумма 1");
-// let expense_2 = prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 2");
-// let expenseAnswer_2 = prompt("Во сколько обойдется", "Сумма 2");
-
-let expenses = {
-    'expense_1': prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 1"),
-    'expenseAnswer_1': prompt("Во сколько обойдется", "Сумма 1"),
-    'expense_2': prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 2"),
-    'expenseAnswer_2': prompt("Во сколько обойдется", "Сумма 2")
-};
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
-    "money": money,
-    "timeData": time,
-    "expenses": expenses,
-    "optionalExpenses": "",
-    "income": "",
-    'saving': false
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false
 };
 
-let onedayMoney = money / 30;
+let expense_1 = prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 1"),
+    expenseAnswer_1 = prompt("Во сколько обойдется", "Сумма 1"),
+    expense_2 = prompt("Введите обязательную статью расходов в этом месяце", "Статья расходов 2"),
+    expenseAnswer_2 = prompt("Во сколько обойдется", "Сумма 2");
+
+appData.expenses.expense_1 = expenseAnswer_1;
+appData.expenses.expense_2 = expenseAnswer_2;
+
+let onedayMoney = appData.budjet / 30;
 alert("Ваш бюджет на 1 день составляет - " + onedayMoney);
